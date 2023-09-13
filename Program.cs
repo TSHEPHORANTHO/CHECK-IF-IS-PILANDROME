7e5c5ee86240bdd336731aacff1718907ea5a16c
Dell-User <Dell-User@DESKTOP-88R8045>
2023-09-13 20:50:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHECK_IF_IS_PILANDROME
+{
+    class Program
+    {
+        static bool IsPalindrome(string word)
+        {
+            int length = word.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+            if (word[i] != word[length - i - 1])
+                {
+            return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter a Palindrome: ");
+            string EnterWord = Console.ReadLine();
+
+
+            if (IsPalindrome(EnterWord))
+            {
+                Console.WriteLine(EnterWord +" is a Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine(EnterWord +" is not a palindrome.");
+            }
+            Console.ReadLine();
+            { 
+        }
+        }
+    }
+}
